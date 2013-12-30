a5a10e83d567f465cd68c3272d1bcdab86d32dc2
johncmolyneux <johncmolyneux@hotmail.com>
2013-12-30 20:03:05 +0000

    Update jquery.extend.js
    Create custom events and assign event handlers with extended version of jQuery on()

--- src/jquery.extend.js
+++ src/jquery.extend.js
@@ -15,36 +15,3 @@ $.fn.on = function () {
     }
     return $.fn._on.apply(this, arguments);
 };
-
-// custom events -------------------------------------------------------------------------------------
-
-// fires when child images are loaded within a selected element
-$.createEvent("imagesloaded", function (element, callback) {
-    var $element = $(element);
-    var $images = $element.find("img");
-    var imgCount = $images.length;
-    if (!imgCount) {
-        callback();
-    } else {
-        $images.each(function () {
-            $(this).one("load error", function () {
-                imgCount--;
-                if (imgCount == 0) {
-                    callback();
-                }
-            });
-            if (this.complete) $(this).load();
-        });
-    }
-});
-
-// fires when the enter key is pressed within a selected element (must accept keyup)
-$.createEvent("enterkey", function (element, callback) {
-    var $element = $(element);
-    $element.on("keyup", function(e) {
-        var code = (e.keyCode ? e.keyCode : e.which);
-        if (code == 13) {
-            callback();
-        }
-    });    
-})
