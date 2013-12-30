60c0f28870122bcc65f5eaefaefe8353363ee011
johncmolyneux <johncmolyneux@hotmail.com>
2013-12-30 19:29:41 +0000

    Update jquery.extend.js

--- src/jquery.extend.js
+++ src/jquery.extend.js
@@ -15,3 +15,36 @@ $.fn.on = function () {
     }
     return $.fn._on.apply(this, arguments);
 };
+
+// custom events -------------------------------------------------------------------------------------
+
+// fires when child images are loaded within a selected element
+$.fn.createEvent("imagesloaded", function (element, callback) {
+    var $element = $(element);
+    var $images = $element.find("img");
+    var imgCount = $images.length;
+    if (!imgCount) {
+        callback();
+    } else {
+        $images.each(function () {
+            $(this).one("load error", function () {
+                imgCount--;
+                if (imgCount == 0) {
+                    callback();
+                }
+            });
+            if (this.complete) $(this).load();
+        });
+    }
+});
+
+// fires when enter key is pressed within a selected element (must accept keyup)
+$.fn.createEvent("enter", function (element, callback) {
+    var $element = $(element);
+    $element.on("keyup", function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 13) {
+            callback();
+        }
+    });    
+})
